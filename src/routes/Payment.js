a1ad4b802f8ce3eaed804e8220174e69eeafa9fa
PedroPiuma <lpap1000@gmail.com>
2022-05-06 02:07:13 -0300

    add modal;add basic body of car payment; create payment body;

--- src/routes/Payment.js
+++ src/routes/Payment.js
@@ -1,7 +1,24 @@
+import { Button, Text, useToast } from "@chakra-ui/react"
+import CardPayment from "../components/CardPayment/CardPayment"
 
 function Payment() {
+    const toast = useToast()
+    if (localStorage.length < 1) return <p>Não há compras na lista</p>
     return (
-        <div>Payment</div>
+        <div>
+            <Text>Seu carrinho de compras</Text>
+            {Object.entries(localStorage).map(e => <CardPayment list={e} />)}
+            <Button size={'xs'} width='fit-content' onClick={() => {
+                localStorage.clear()
+                toast({
+                    title: `Carrinho limpo com sucesso`,
+                    status: 'success',
+                    isClosable: true,
+                })
+            }
+            }>Limpar Carrinho</Button>
+            <Button>Finalizar Compra</Button>
+        </div>
     )
 }
 
